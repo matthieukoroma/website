94329a95b8e40740a1e2f67d584f3af9711bc904
matthieu <matthieu.koroma@ens-paris-saclay.fr>
2025-07-21 10:33:39 +0200

    changes expertise

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -50,21 +50,21 @@ function Expertise() {
                 <div className="skill">
                     <h3>Protocole & Analyse</h3>
                     <p>
-                        Faire de votre question de recherche un paradigme scientifique; j'utilise l'analyse conceptuelle et la connaissance bibliographique pour établir un paradigme qui répond à votre question de recherche.
+                        Faire d'une question de recherche un paradigme scientifique; j'utilise l'analyse conceptuelle et la connaissance bibliographique pour établir un paradigme qui répond à une question de recherche.
                     </p>
                 </div>
 
                 <div className="skill">
                     <h3>Projet & Financement</h3>
                     <p>
-                        Transformer votre paradigme scientifique en programme de financement; j'évalue la pertinence et les champs d'application de votre paradigme pour structurer une stratégie qui assure le financement de votre recherche.
+                        Transformer un paradigme scientifique en programme de financement; j'évalue la pertinence et les champs d'application d'un paradigme scientifique pour structurer une stratégie qui assure un financement de recherche.
                     </p>
                 </div>
 
                 <div className="skill">
                     <h3>Ouverture & Interdisciplinarité</h3>
                     <p>
-                        Étendre votre programme de financement en champ interdisciplinaire; j'offre une approche systémique sur l'impact de vos découvertes pour identifier le réseau d'acteurs qui favorise le décloisonnement de votre secteur.
+                        Étendre un programme de financement en champ interdisciplinaire; j'offre une approche systémique dans le cadre de la science ouverte pour identifier le réseau d'acteurs qui favorise le décloisonnement de vos découvertes.
                     </p>
                 </div>
             </div>
